6005cf782a7d98d4fe72414ecb19d981f6431a60
nysat <nysa-cere3@hotmail.com>
2023-08-28 01:48:03 -0400

    added the structure to the README file and some more content to the array in index.js

--- index.js
+++ index.js
@@ -8,63 +8,77 @@ const generateMarkdown = require("./utils/generateMarkdown")
 const questions = [
     {
         type: 'input',
-        name: 'Title',
+        name: 'title',
         message: 'What is the name of this project?'
     },
     {
         type: 'input', 
-        name: 'Description',
+        name: 'description',
         message: 'Provide a short description explaining what was your motivation, why did you build this project, what problem does it solve and what did you learn?'
     }, 
     {
         type: 'input',
-        name: 'Table of contents',
+        name: 'screenshot',
+        message: 'Provide a path to the image you want to use as the screenshot.'
+    },
+    {
+        type: 'input',
+        name: 'link',
+        message:'Please provide a URL where a user can access the deployed application'
+    },
+    {
+        type: 'input',
+        name: 'table of contents',
         message: 'Table of contents'
     },
     {
         type: 'input',
-        name: 'Installation',
+        name: 'installation',
         message: 'What does the user need to install to run this app? (ex: dependencies)',
     },
     {
         type: 'input',
-        name: 'Usage',
+        name: 'usage',
         message: 'How is this app used?',
     },
     {
         type: 'input',
-        name: 'Contributing',
+        name: 'contributing',
         message: 'Who contruibuted to this project? (List github username and email.)'
     },
     {
-        type: 'input',
-        name: 'License',
-        message: 'What license is being used?'
+        type: 'checkbox',
+        name: 'license',
+        message: 'What license is being used? (Please select one applicable to this project.)',
+        choices: ["MIT", "Berkeley Software Distribution License (BSD)", "GNU General Public License (GNU GPL)", "Apache License 2.0", "Internet Systems Consortium (ISC) License", "NONE"]
+
     },
     {
         type: 'input',
-        name: 'Test',
+        name: 'test',
         message: 'Write tests for your application then provide examples on how to run them here'
     },
     {
         type: 'input',
-        name: 'Questions',
+        name: 'questions',
         message: 'Refer to contact info'
     },
     {
         type: 'input',
-        name: 'Username',
+        name: 'username',
         message: 'What is your github username?',
     },
     {
         type: 'input',
-        name: 'Email',
+        name: 'email',
         message: 'What is your email',
     },
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    
+}
 
 // TODO: Create a function to initialize app
 function init() {}
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -10,7 +10,10 @@ function renderLicenseBadge(license) {
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  
+  if (license !== 'none') {
+    return `\n * [License](#license)\n*`
+  }
+
 }
 
 // TODO: Create a function that returns the license section of README
@@ -21,6 +24,32 @@ function renderLicenseSection(license) {}
 function generateMarkdown(data) {
   return `# ${data.title}
 
+## Description
+${data.description}
+##### Deployed Application
+${data.link}
+##### Screenshot
+![alt-text](${data.screenshot})
+## Table of contents
+* [Description](#description)
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contributing](#contributing)
+* [License](#license)
+* [Test](#test)
+* [Questions](#questions)
+## Installation 
+${data.installation}
+## Usage
+How to use this application: ${data.usage}
+## Contributing
+${data.contributing}
+## License
+${data.license}
+## Testing 
+${data.test}
+## Questions 
+If there is any questions please send your questions [here](${data.email}) or visit [github/${data.username}](https://github.com/${data.username}).
 `;
 }
 
