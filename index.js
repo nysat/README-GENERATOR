086ebebd09ab1d0f6efda8f8c5d5aa9b5a6f0a51
nysat <nysa-cere3@hotmail.com>
2023-08-28 02:03:33 -0400

    added a function to initialize the app

--- index.js
+++ index.js
@@ -77,11 +77,15 @@ const questions = [
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    
+    fs.writeFile(fileName, data) 
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((responses) => {
+        console.log ('Creating README.md file...');
+        writeToFile('./result/README.md', generateMarkdown({...responses}));
+    });
+}
 
-// Function call to initialize app
 init();
